896930582871c90b65966a4553b2f434fbac8b83
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-02-09 22:16:04 +0100

    initizlizing project

---
+++ GHData/src/main/java/sebastian/GHData/GhDataApplication.java
@@ -0,0 +1,13 @@
+package sebastian.GHData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhDataApplication.class, args);
+	}
+
+}
