6133f6417700cb727e59adc8b05bcb7e7d6474d9
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-02-12 22:50:00 +0100

    using modern Java approach

--- GHData/src/main/java/sebastian/GHData/response/external/RepoObject.java
+++ GHData/src/main/java/sebastian/GHData/response/external/RepoObject.java
@@ -1,17 +1,24 @@
 package sebastian.GHData.response.external;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
 
+import java.util.HashMap;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RepoObject {
+
     private String repositoryName;
     private String ownerLogin;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String fork;
     private String language;
     private int size;
+    private HashMap<String, String> branches;
 
     @JsonProperty("name")
     public void setName(String name) {
@@ -23,6 +30,11 @@ public class RepoObject {
         this.ownerLogin = ownerNode.get("login").asText();
     }
 
+    @JsonProperty("fork")
+    public void setFork(String fork) {
+        this.fork = fork;
+    }
+
     @JsonProperty("language")
     public void setLanguage(String language) {
         this.language = language;
@@ -31,5 +43,9 @@ public class RepoObject {
     @JsonProperty("size")
     public void setName(Integer size) {
         this.size = size;
+    }
+
+    public void setBranchesMap(HashMap<String, String> branches) {
+        this.branches = branches;
     }
 }
--- GHData/src/main/java/sebastian/GHData/service/GetAndMapData.java
+++ GHData/src/main/java/sebastian/GHData/service/GetAndMapData.java
@@ -9,13 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import sebastian.GHData.resource.repoBranchesRequester;
 import sebastian.GHData.resource.repoListRequester;
-import sebastian.GHData.response.external.GitHubRestApiReposListResponse;
 import sebastian.GHData.response.external.RepoObject;
 import sebastian.GHData.response.inner.UserNotFound;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class GetAndMapData {
@@ -25,47 +23,72 @@ public class GetAndMapData {
 
             // request all user's repositories from GitHub API
             String jsonReposResponse = repoListRequester.sendGetRequest(username);
-            ObjectMapper objectMapper = new ObjectMapper();
 
+            assert jsonReposResponse != null;
             if (jsonReposResponse.equals("User not found")) {
-                String notFoundResponse = objectMapper.writeValueAsString(new UserNotFound(404, jsonReposResponse));
-
-                ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .header("Content-Type", "application/json")
-                        .body(notFoundResponse);
-
-                return responseEntity;
-            } else if (jsonReposResponse.equals("The limit of requests for the GitHub API from this IP address has been reached")) {
-                String requestsLimitResponse = objectMapper.writeValueAsString(new UserNotFound(403, jsonReposResponse));
-
-                ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
-                        .header("Content-Type", "application/json")
-                        .body(requestsLimitResponse);
-
-                return responseEntity;
+                return userNotFound(jsonReposResponse);
+            } else if (jsonReposResponse.equals("The limit of requests")) {
+                return requestsLimit(jsonReposResponse);
             }
 
             ObjectMapper mapper = new ObjectMapper();
-            List<RepoObject> repoList = mapper.readValue(jsonReposResponse, new TypeReference<List<RepoObject>>(){});
 
-            for (RepoObject obj : repoList) {
-                System.out.println(obj.toString());
-            }
+            List<RepoObject> modifiedRepos = mapper.readValue(jsonReposResponse, new TypeReference<List<RepoObject>>(){})
+                    .stream()
+                    .filter(repo -> repo.getFork().equals("false"))
+                    .peek(repo -> repo.setFork(null))
+                    .map(repo -> {
+                        String repoResponse = repoBranchesRequester.sendGetRequest(repo.getOwnerLogin(), repo.getRepositoryName());
+                        ObjectMapper mapperBranch = new ObjectMapper();
+                        try {
+                            JsonNode jsonBranches = mapperBranch.readTree(repoResponse);
+                            HashMap<String, String> branchesMap = new HashMap<>();
+                            for (JsonNode branch : jsonBranches) {
+                                branchesMap.put(branch.get("name").asText(), branch.get("commit").get("sha").asText());
+                            }
+                            repo.setBranchesMap(branchesMap);
+                            return repo;
+                        } catch (JsonProcessingException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toList());
 
-            //List<RepoObject> repoList = new ArrayList<>();
 
             // Converting data to JSON
-            String jsonString = objectMapper.writeValueAsString(repoList);
+            String jsonResponse = mapper.writeValueAsString(modifiedRepos);
 
             ResponseEntity<String> responseEntity = ResponseEntity.ok()
                     .header("Content-Type", "application/json")
-                    .body(jsonString);
+                    .body(jsonResponse);
 
             return responseEntity;
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             return new ResponseEntity<>("Error with converting JSON", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public ResponseEntity<String> userNotFound(String json) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String notFoundResponse = mapper.writeValueAsString(new UserNotFound(404, json));
+
+        ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .header("Content-Type", "application/json")
+                .body(notFoundResponse);
+
+        return responseEntity;
+    }
+
+    public ResponseEntity<String> requestsLimit(String json) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String requestsLimitResponse = mapper.writeValueAsString(new UserNotFound(403, json));
+
+        ResponseEntity<String> responseEntity = ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .header("Content-Type", "application/json")
+                .body(requestsLimitResponse);
+
+        return responseEntity;
     }
 
 }
